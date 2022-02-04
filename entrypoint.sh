02a092b66b8d8531bdb844db9e9ec66837b225b3
Marcel Boldt <mboldt@confluent.io>
2022-02-04 15:13:38 +0100

    process env vars in topology and client.config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,13 @@
 pwd
 ls -l
 
+envsubst < $2 > /client.config
+envsubst < $3 > /topology.yml
+
 kafka-topology-builder.sh --version
 echo ""
 kafka-topology-builder.sh \
     --brokers $1 \
-    --clientConfig $2 \
-    --topology $3 \
+    --clientConfig /client.config \
+    --topology /topology.yml \
     --dryRun
