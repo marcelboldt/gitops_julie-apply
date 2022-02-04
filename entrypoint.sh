7132d70ee17424ef199dbca6d32ad84cbc90822a
Marcel Boldt <mboldt@confluent.io>
2022-02-04 17:29:51 +0100

    no dryRun

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-pwd
-ls -l
-
 envsubst < $2 > /client.config
 envsubst < $3 > /topology.yml
 
-julie-ops-cli.sh --version
+echo "JulieOps version $(julie-ops-cli.sh --version)"
 echo ""
 julie-ops-cli.sh \
     --brokers $1 \
     --clientConfig /client.config \
-    --topology /topology.yml \
-    --dryRun
+    --topology /topology.yml
